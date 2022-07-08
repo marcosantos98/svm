4c50f803fee2fdb6cd63e615f53bdb500cea2c35
mscode98 <workmarco98@gmail.com>
2022-07-08 16:10:34 +0100

    Option to only run recorded tests.

--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ cd examples
 
 FAILED=0
 PASS=0
+NOT_RECORDED=0
 
 for i in *.svm;
 do
@@ -18,6 +19,26 @@ do
     then
         echo "Recording $i"
         ../svm $i > $i.out
+    elif [ "$1" = "-or" ]
+    then
+        FILE=$i.out
+        if test -f "$FILE"; 
+        then
+            echo "Running $i"
+            run="$(../svm $i)"
+            out="$(cat $i.out)"
+            if [ "$run" = "$out" ]
+            then
+                echo "  PASS: $i"
+                ((PASS=PASS+1))
+            else
+                echo "  FAIL: $i"
+                ((FAILED=FAILED+1))
+            fi
+        else
+            echo "  Not recorded: $i"
+            ((NOT_RECORDED=NOT_RECORDED+1))
+        fi
     else
         FILE=$i.out
         if test -f "$FILE"; 
@@ -34,7 +55,7 @@ do
                 ((FAILED=FAILED+1))
             fi
         else
-            echo "Run './test.sh rec' before running the tests."
+            echo "Run './test.sh rec' before running the tests or run './test.sh -or'."
             exit 1
         fi
         
@@ -42,9 +63,10 @@ do
 done
 
 echo ""
-echo "Results from $((PASS+FAILED)) tests:"
+echo "Results from $((PASS+FAILED+NOT_RECORDED)) tests:"
 echo "Pass: $PASS"
 echo "Failed: $FAILED"
+echo "Not recorded: $NOT_RECORDED"
 echo ""
 
 cd ..
